14364a61c0b3123225d492a18d279c697f86c336
FrederikString <82048168+FrederikString@users.noreply.github.com>
2021-07-14 11:34:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=['GET'])
+def Frontend(): 
+    return '''
+<html>
+    <body>
+    <center>
+     <div style = "margin-top: 10%">
+     <h1>Anmeldung</h1>
+     <p>Wenn Sie Interesse an unserem Produkt haben und mit uns in Kontakt treten wollen,<br>
+       können Sie sich hier anmelden, indem Sie ihre eMail-Adresse hinterlassen. </p> 
+       <p> Es haben sich bisher X Personen bei uns angemeldet. </p> <br>
+        <form action = "http://127.0.0.1:5000/Anmeldung", method = "POST">
+        <label for="email">eMail-Adresse:</label>
+        <input type="text" name="email"><br><br>
+        <input type="submit" value="Anmelden">
+        </form>
+     </div>
+    </body>
+</html>'''
